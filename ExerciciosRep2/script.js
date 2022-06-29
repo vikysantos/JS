649f11b0f14abeed57e27326cb6afea531b1edba
Vitalia Santos <52931273+vikysantos@users.noreply.github.com>
2022-06-29 15:09:17 +0100

    atualizações exercicios aulas, exer netflix, inicio de games

--- ExerciciosRep2/script.js
+++ ExerciciosRep2/script.js
@@ -1,22 +1,39 @@
-function gerar(){
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
-let num = document.getElementById('numero')
-let tab = document.getElementById('tab')
-
-if(num.value.length == 0){
-    alert('[ERRO] ')
-} else { 
-    let n = Number(num.value)
-    tab.innerHTML = ''
-    for(let c = 1 ; c <= 10 ; c++){
-
-        let item = document.createElement('option')
-        item.text = `${n} x ${c} = ${n*c}`
-        item.value = `tab${c}`
-        tab.appendChild(item)
+function isNumber(n){                                                       // verificar se o mumero esta dentro do intervalo 
+    if(Number(n) >= 1 && Number <= 100){
+        return true
+    }else {
+        return false
     }
-    
 }
+
+function inList(n, l){                                                      // Verificar se o numero está na lista 
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else {
+        return false
+    }
 
 }
 
+function adicionar(){
+    if(isNumber(num.value) && !inList(num.value, valores)){
+        valores.push(Number(num.value))                                     //push é para adicionar valor no array
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        
+    } else { 
+        alert('[ERRO] Valor não está correto ou já está na lista!')
+        
+    }
+
+}
+/*
+function finalizar(){
+
+}*/
